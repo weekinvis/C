9f43df050eb25412d716cfede18ad4c8b8ee08b5
weekinvis <89652278+weekinvis@users.noreply.github.com>
2024-03-31 11:07:27 -0300

    Add files via upload
    I'll try to make the code more effective in order to calculate faster, but for a first code, I think this is ok. It's a extreme simple code btw.

---
+++ collatzConjectureCalculator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define SUCESS 0
+
+int main(int agrc , char ** agrv) {
+    
+    int number, step;
+    printf("Insert the number:");
+    scanf("%i", &number);
+
+    while(number > 1) {
+        
+        if(number % 2 == 0) {
+            number = number / 2;
+            step = step + 1;
+            printf("The result of step n°%i is %i\n", step, number);
+    }   else {
+            number = 3 * number + 1;
+            step = step + 1;
+            printf("The result of step n°%i is %i\n", step, number);
+        } 
+    }
+    printf("Sucess\n");
+    return SUCESS;
+}
