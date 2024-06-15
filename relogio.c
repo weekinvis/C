c3024f25cc98dbd81cd2ab7f44639d6aef343711
weekinvis <89652278+weekinvis@users.noreply.github.com>
2024-06-15 10:05:02 -0300

    Add files via upload

---
+++ relogio.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int H, M, S, T;
+
+    scanf("%i %i %i %i", &H, &M, &S, &T);
+
+    S += T;
+
+    while(S >= 60) {
+        M += 1;
+        S -= 60;
+    }
+    while(M >= 60) {
+        H += 1;
+        M -= 60;
+    }
+    while(H >= 24) {
+        H -= 24;
+    }
+    printf("%i\n%i\n%i", H, M, S);
+
+}
