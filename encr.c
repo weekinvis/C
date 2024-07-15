e4a893fe990ec9d5225973fa440affecbadad4c1
weekinvis <89652278+weekinvis@users.noreply.github.com>
2024-07-15 17:08:32 -0300

    Add files via upload

---
+++ encr.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int qLetras, i, j = 0;
+    char* letras;
+    uint64_t fator = 1;
+    uint64_t soma = 0;
+
+    printf("Digite quantas letras vc precisa\n:");
+    scanf("%d%*c", &qLetras);
+
+    letras = (char*)calloc(qLetras + 1, sizeof(char));
+
+    printf("Digite a string: ");
+    fgets(letras, qLetras + 1, stdin);    
+
+    for(i = 0; qLetras > i; i++) {
+        if(j >= sizeof(uint64_t)) {
+            j = 0;
+            printf("%lli\n", soma);
+            soma = 0;
+        }
+        fator = pow(2, (8 * j));
+        j++;
+        soma += letras[i] * fator;
+    }
+    free(letras);
+    printf("%lli\n", soma);
+}
