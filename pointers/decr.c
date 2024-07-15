2e0367431aaa73ea91df0a7418581a3ed54c65d2
weekinvis <89652278+weekinvis@users.noreply.github.com>
2024-07-15 17:09:27 -0300

    Add files via upload

---
+++ pointers/decr.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+    uint64_t* numeros;
+    char* decript;
+    int qntNum;
+    int i, j = 0;
+
+    printf("Digite a quantidade de numeros:\n:");
+    scanf("%d", &qntNum);
+
+    numeros = (uint64_t*)calloc(qntNum, sizeof(uint64_t));
+
+    for(i = 0; qntNum > i; i++) {
+        printf("Digite o numero %i: ", i + 1);
+        scanf("%*c%lli", &numeros[i]);
+    }
+
+    for(i = 0; qntNum > i; i++) {
+        decript = (char*)&numeros[i];
+        
+        for(j = 0; sizeof(uint64_t) > j; j++) {
+            printf("%c", *(decript + j));
+        }
+        
+    }
+    free(numeros);
+    return 0;
+}
