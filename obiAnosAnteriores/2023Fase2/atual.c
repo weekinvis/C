d15198fff2b09fe3d232676dbdd8ef202f23052a
weekinvis <89652278+weekinvis@users.noreply.github.com>
2024-07-26 11:22:46 -0300

    Add files via upload
    incompleto, sera finalizado dps

---
+++ obiAnosAnteriores/2023Fase2/atual.c
@@ -0,0 +1,29 @@
+#include <stdlib.h> // para usar alocacao dinamica
+#include <stdio.h> // para usar scanf e printf
+
+int main(int argc, char* argv[]) {
+	int* seq, * seq2;
+	int a, i, j, k;
+	int count[2] = { 0, 0 };
+
+	scanf_s("%d", &a);
+
+	seq2 = (int*)malloc(sizeof(int) * a);
+	seq = (int*)malloc(sizeof(int) * a);
+
+	for (i = 0; a > i; i++) {
+		scanf_s("%d", &seq[i]);
+	}
+	printf("\n");
+
+	for (i = 0; a > i; i++) {
+		for (j = i; a > j; j++) {
+			seq2[j] = seq[j];
+		}
+	}
+
+	free(seq);
+	free(seq2);
+
+	return 0;
+}
