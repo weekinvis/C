55724633e15f036e90817046b95884b3e1d13917
weekinvis <89652278+weekinvis@users.noreply.github.com>
2024-12-08 14:41:51 -0300

    Add files via upload

---
+++ 2024/dados/dados.c
@@ -0,0 +1,74 @@
+#include "dados.h"
+#include "util.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define VALOR_MINIMO 1
+
+uint8_t leDados(int* qnt, int* nFacesDado) {
+    uint8_t scan;
+
+    printf("Digite os dados: ");
+    scan = scanf("%d%*c%d%*c", qnt, nFacesDado);
+
+    return scan;
+
+}
+
+uint8_t geraNumeroDado(int nFacesDado) {
+    return (rand() % nFacesDado) + VALOR_MINIMO;
+}
+
+uint8_t* girarDados(int qnt, int nFacesDado) {
+    uint8_t* resultados = NULL;
+
+    resultados = (uint8_t*)malloc(sizeof(uint8_t) * qnt + 1);
+    resultados[0] = qnt + 1;
+
+    for(int p = 1; p < qnt + 1; p++) {
+        resultados[p] = geraNumeroDado(nFacesDado);
+    }
+
+    return resultados;
+
+}
+
+void imprimeResultadosDados(uint8_t* resultados) {
+    uint8_t* indicesMaioresValores = NULL;
+    uint16_t soma = 0;
+    int q = 1;
+
+    indicesMaioresValores = utilEncontraMaioresValores(resultados);
+
+    system("cls || clear");
+
+    printf("[");
+
+    for(int p = 1; p < resultados[0]; p++) {
+
+        soma += resultados[p];
+        
+        if(p == indicesMaioresValores[q] && q < indicesMaioresValores[0]) {
+            printf("\033[94m");
+        }
+
+        p + 1 == resultados[0] ? printf("%hhu\033[0m] < Soma = %hu", resultados[p], soma) : printf("%hhu, ", resultados[p]);
+
+        if(p == indicesMaioresValores[q] && q < indicesMaioresValores[0]) {
+            printf("\033[0m");
+            q++;
+        }
+
+    }
+    printf("\n");
+
+    free(indicesMaioresValores);
+
+    return;
+}
+
+void liberaResultadosDados(uint8_t* resultados) {
+    free(resultados); 
+}
---
+++ 2024/dados/utilDados.c
@@ -0,0 +1,60 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "util.h"
+
+uint8_t* utilEncontraMaioresValores(uint8_t* resultados) {
+    uint8_t maiorValor;
+
+    uint8_t* indices = NULL;
+    uint8_t qntIndices = 0;
+
+    maiorValor = resultados[1];
+
+    for(int p = 1; p < resultados[0]; p++) {
+        if(resultados[p] > maiorValor) {
+            maiorValor = resultados[p];
+        }
+    }
+
+    for(int p = 1; p < resultados[0]; p++) {
+        if(resultados[p] == maiorValor) {
+            qntIndices++;
+            indices = (uint8_t*)realloc(indices, sizeof(uint8_t) * qntIndices + 1);
+            indices[qntIndices] = p;
+        }
+    }
+
+    indices[0] = qntIndices + 1;
+
+    return indices;
+}
+
+void utilMenuInicial() {
+    system("clear || cls");
+    printf("Bem vindo ao programa!\n");
+    printf("Modo de usar: {quantidade de dados}d{num de faces do dado}\nEx: (3d20) < 3 dados de 20 faces\n");
+    printf("Para sair, digite qualquer caractere e de enter!\n");
+    printf("Nao passe, de nenhum modo, de 254. Seja por 254 lancamentos ou por dados maiores do que 254 faces.\n\n");
+}
+
+bool utilVerificaErro(uint8_t scan, int qnt, int nFacesDado) {
+    fflush(stdin);
+        
+    if(scan != 2) {
+        fprintf(stderr, "Digite conforme especificado! {qntDados}d{nFacesDado}!\n");
+        return true;
+    }
+    if((0 >= qnt) || (0 >= nFacesDado)) {
+        fprintf(stderr, "Digite um numero positivo para quantidade ou numero de faces!\n\n");
+        return true;
+    }
+    if((qnt >= LIMITE_MEMORIA) || (nFacesDado >= LIMITE_MEMORIA)) {
+        fprintf(stderr, "Limite de memoria alcancado!\n\n");
+        return true;
+    }
+
+    return false;
+        
+}
