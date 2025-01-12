90ab5bf1d4e1168b7886223b882fa64ba1c16abc
weekinvis <89652278+weekinvis@users.noreply.github.com>
2025-01-12 12:32:21 -0300

    Delete 2024 directory

--- 2024/recuperacaoFinal/pt2/1.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef bdop$1_h__
-#define bdop$1_h__
-
-#include "util.h"
-
-void cadastrarReceita(receita_t** receitas, int* qntReceitas);
-
-#endif
--- 2024/recuperacaoFinal/pt2/2.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef bdop$2_h__
-#define bdop$2h_h__
-
-#include "util.h"
-
-void imprimirReceitas(receita_t* receitas, int qntReceitas);
-
-#endif
